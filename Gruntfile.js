5485746dc4ed950238549d01d9232c9f8bfe63a5
nscarcella <Nicolas Scarcella>
2015-04-11 21:46:07 -0300

    compiling and concatening styles with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,30 +10,60 @@ module.exports = function(grunt) {
     },
 
     jade: {
-        compile: {
-          files: [
-            {
-              cwd: "app/views",
-              src: "**/*.jade",
-              dest: "app/views",
-              expand: true,
-              ext: ".html"
-            },
-            {
-              cwd: "",
-              src: "index.jade",
-              dest: "",
-              expand: true,
-              ext: ".html"
-            }
-          ]
+      compile: {
+        files: [
+          {
+            cwd: 'app/views',
+            src: '**/*.jade',
+            dest: 'app/views',
+            expand: true,
+            ext: '.html'
+          },
+          {
+            cwd: '',
+            src: 'index.jade',
+            dest: '',
+            expand: true,
+            ext: '.html'
+          }
+        ]
+      }
+    },
+
+    concat: {
+      styles: {
+        src: 'app/views/**/*.scss',
+        dest: 'styles.scss'
+      }    
+    },
+
+    sass: {
+      dist: {
+        files: {
+          'styles.css': 'styles.scss'
         }
+      }
+    },
+
+    clean: {
+      compile: ['styles.css'],
+    },
+
+    compile: {
+      html: ['jade'],
+      styles: ['concat:styles', 'sass', 'clean:compile']
     }
 
   });
 
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.registerTask('test', ['karma']);
+  grunt.registerMultiTask('compile', function() {
+    grunt.task.run(this.data);
+  });
 };
